711c2ee87306a5dddf44cd75fa6447a6c6f0e9f2
MsVee55 <precious.v.augustine@gmail.com>
2023-04-04 09:27:07 -0700

    POST an email

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status."""
-if _name_ == "_main_":
+if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
