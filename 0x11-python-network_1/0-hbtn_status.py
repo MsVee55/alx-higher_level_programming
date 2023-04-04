b58f6f92e959645fcf1e4c76e83fdbcaa9f4b740
MsVee55 <precious.v.augustine@gmail.com>
2023-04-04 07:56:20 -0700

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status."""
-
-if __name__ == '__main__':
+if _name_ == "_main_":
     import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
+                content = response.read()
 
-        as response:
-print('Body response:')
-print('\t- type: {}'.format(type(content)))
-print('\t- content: {}'.format(content))
-print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+                print('Body response:')
+                print('\t- type: {}'.format(type(content)))
+                print('\t- content: {}'.format(content))
+                print('\t- utf8 content: {}'.format(content.decode("utf-8", "replace")))
