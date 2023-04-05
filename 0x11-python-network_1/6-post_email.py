f188421f40763c87d12747a870f2b17a46891204
MsVee55 <precious.v.augustine@gmail.com>
2023-04-04 18:18:01 -0700

    Network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,7 @@ import requests
 import sys
 
 
-if __name__ == '__main__":
+if __name__ == "__main__":
     email = {'email': sys.argv[2]}
     r = requests.post(sys.argv[1], data=email)
     print(r.text)
